0cdc4a7c248d46a3a9ece94bcca9366eeb4a6184
AhmedSaber <ahmedomani2003@gmail.com>
2024-12-21 15:18:44 +0200

    change solution

--- app/server.go
+++ app/server.go
@@ -6,51 +6,22 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+var _ = net.Listen
+var _ = os.Exit
 
-	// Start listening on the required port
+func main() {
+	
+	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	fmt.Println("Server is listening on port 4221")
-
-	for {
-		// Accept an incoming connection
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			continue
-		}
-
-		// Handle the connection
-		handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	// Read the incoming request
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
+	
+	connection , err := l.Accept()
 	if err != nil {
-		fmt.Println("Error reading from connection:", err.Error())
-		return
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	}
-	fmt.Printf("Received request:\n%s\n", string(buffer[:n]))
-
-	// Send a basic HTTP 200 OK response
-	response := "HTTP/1.1 200 OK\r\n" +
-		"Content-Length: 0\r\n" +
-		"\r\n"
-
-	_, err = conn.Write([]byte(response))
-	if err != nil {
-		fmt.Println("Error writing response:", err.Error())
-		return
-	}
-	fmt.Println("Response sent")
+	connection.Write([]byte("HTTP/1,1 200 OK\r\n\r\n"))
 }
