997f91b38959d7a0946ccc6f5f67850ba0a8934a
AhmedSaber <ahmedomani2003@gmail.com>
2024-12-21 15:00:34 +0200

    change the port

--- app/server.go
+++ app/server.go
@@ -13,13 +13,11 @@ var _ = os.Exit
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	l, err := net.Listen("tcp", "0.0.0.0:8080")
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 8080")
+		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	fmt.Println("A7aaaa")
 	
 	_, err = l.Accept()
 	if err != nil {
