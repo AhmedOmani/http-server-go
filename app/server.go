3e641c2eb74fded8759843b2d8b40c39ae526eab
AhmedSaber <ahmedomani2003@gmail.com>
2024-12-22 07:50:33 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -3,54 +3,65 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 	"strings"
 )
-
-var _ = net.Listen
-var _ = os.Exit
 
 func handleRequest(connection net.Conn) {
 
 	defer connection.Close()
 
 	buffer := make([]byte, 4096)
+	//take the request as stream of bytes
 	n, err := connection.Read(buffer)
 
 	if err != nil {
-		fmt.Println("Error reading request:", err)
+		fmt.Println("error during read the http request!")
 		return
 	}
 
+	//convert the stream of bytes to string
 	httpRequest := string(buffer[:n])
 
 	lines := strings.Split(httpRequest, "\r\n")
 
 	if len(lines) < 1 {
-		fmt.Println("Invlid request structure", err)
+		fmt.Println("so bad format of request")
 		return
 	}
-
 	requestLine := lines[0]
 	requestParts := strings.Split(requestLine, " ")
 
 	if len(requestParts) < 3 {
-		fmt.Println("Invalid request format", err)
+		fmt.Println("Invalid request structure", err)
 		return
 	}
-
 	method, url, httpVersion := requestParts[0], requestParts[1], requestParts[2]
 
-	if method == "GET" && url == "/" {
-		handleGET(connection, httpVersion)
+	if method == "GET" {
+		s := strings.Split(url, "/")
+		if len(s) == 2 && s[1] == "" { 
+			handleRoot(connection , httpVersion)
+		} else if len(s) >= 2 && s[1] == "echo" {
+			var body string
+			if len(s) > 2 {
+				body = s[2]
+			} 
+			handleGET(connection , httpVersion , body) 
+		} else {
+			handleNotFound(connection, httpVersion)
+		}
+		
 	} else {
 		handleNotFound(connection, httpVersion)
 	}
 
 }
-
-func handleGET(connection net.Conn, httpVersion string) {
-	response := fmt.Sprintf("%s 200 OK\r\n\r\n", httpVersion)
+func handleRoot(connection net.Conn , httpVersion string) {
+	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n", httpVersion)
+	connection.Write([]byte(response))
+}
+func handleGET(connection net.Conn, httpVersion string , body string) {
+	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion , len(body) , body)
 	connection.Write([]byte(response))
 }
 
@@ -63,21 +74,18 @@ func main() {
 
 	fmt.Println("Logs from your program will appear here!")
 
-	//create an tcp server !
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("A7a mfesh connection: ", err)
+		fmt.Println("Can not bind the port for tcp server")
 		return
 	}
 
-	//wait for a client connection
 	for {
 		connection, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			continue
+			fmt.Println("cant accept this connection")
+			return
 		}
-		go handleRequest(connection)
+		handleRequest(connection)
 	}
-
 }
