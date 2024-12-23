7156f40539dc40e04df0a9044b3cc526ec069475
AhmedSaber <ahmedomani2003@gmail.com>
2024-12-23 20:18:57 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,6 +6,20 @@ import (
 	"strings"
 )
 
+func getUserAgent(lines []string) string {
+	var userAgent string 
+	for _ , line := range lines {
+		if strings.HasPrefix(line , "User-Agent:") {
+			parts := strings.SplitN(line , " " , 2)
+			if len(parts) == 2 {
+				userAgent = parts[1]
+			}
+			break 
+		}
+	}
+	return userAgent 
+}
+
 func handleRequest(connection net.Conn) {
 
 	defer connection.Close()
@@ -28,6 +42,10 @@ func handleRequest(connection net.Conn) {
 		fmt.Println("so bad format of request")
 		return
 	}
+	
+	//Get userAgent
+	userAgent := getUserAgent(lines)
+
 	requestLine := lines[0]
 	requestParts := strings.Split(requestLine, " ")
 
@@ -35,38 +53,54 @@ func handleRequest(connection net.Conn) {
 		fmt.Println("Invalid request structure", err)
 		return
 	}
-	method, url, httpVersion := requestParts[0], requestParts[1], requestParts[2]
 
-	if method == "GET" {
-		s := strings.Split(url, "/")
-		if len(s) == 2 && s[1] == "" { 
-			handleRoot(connection , httpVersion)
-		} else if len(s) >= 2 && s[1] == "echo" {
-			var body string
-			if len(s) > 2 {
-				body = s[2]
-			} 
-			handleGET(connection , httpVersion , body) 
-		} else {
-			handleNotFound(connection, httpVersion)
-		}
-		
-	} else {
-		handleNotFound(connection, httpVersion)
+	method, url, httpVersion := requestParts[0], requestParts[1], requestParts[2]
+	
+	if method != "GET" {
+		handleNotFound(connection , httpVersion)
+		return 
 	}
 
+	if strings.HasPrefix(url , "/echo/") {
+		body := strings.TrimPrefix(url , "/echo/")
+		handleGETEcho(connection , httpVersion , body)
+		return 
+	}
+
+	if strings.HasPrefix(url , "/user-agent") {
+		handleGETUserAgent(connection , httpVersion , userAgent)
+		return
+	}
+
+	if strings.HasPrefix(url , "/") {
+		body := strings.TrimPrefix(url , "/")
+		if body != "" {
+			handleNotFound(connection , httpVersion)
+		}
+		handleGETRoot(connection , httpVersion)
+	}
+
+	handleNotFound(connection , httpVersion)
+	
 }
-func handleRoot(connection net.Conn , httpVersion string) {
+
+func handleGETRoot(connection net.Conn , httpVersion string) {
 	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n", httpVersion)
 	connection.Write([]byte(response))
 }
-func handleGET(connection net.Conn, httpVersion string , body string) {
-	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion , len(body) , body)
+
+func handleGETUserAgent(connection net.Conn , httpVersion string , userAgent string) {
+	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion , len(userAgent) , userAgent)
 	connection.Write([]byte(response))
 }
 
 func handleNotFound(connection net.Conn, httpVersion string) {
 	response := fmt.Sprintf("%s 404 Not Found\r\n\r\n", httpVersion)
+	connection.Write([]byte(response))
+}
+
+func handleGETEcho(connection net.Conn ,httpVersion string , body string) {
+	response := fmt.Sprintf("%s 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", httpVersion , len(body) , body)
 	connection.Write([]byte(response))
 }
 
